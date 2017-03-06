07f2a4f3a1230ea3c1ddd6026aea6a527c131166
Ricky Wu <rickywu1999@gmail.com>
2017-03-05 19:04:23 -0500

    finished code, have to test now

--- parser.py
+++ parser.py
@@ -20,12 +20,6 @@ The file follows the following format:
 	 rotate: create a rotation matrix,
 	    then multiply the transform matrix by the rotation matrix -
 	    takes 2 arguments (axis, theta) axis should be x, y or z
-	 yrotate: create an y-axis rotation matrix,
-	    then multiply the transform matrix by the rotation matrix -
-	    takes 1 argument (theta)
-	 zrotate: create an z-axis rotation matrix,
-	    then multiply the transform matrix by the rotation matrix -
-	    takes 1 argument (theta)
 	 apply: apply the current transformation matrix to the 
 	    edge matrix
 	 display: draw the lines of the edge matrix to the screen
@@ -38,4 +32,84 @@ The file follows the following format:
 See the file script for an example of the file format
 """
 def parse_file( fname, points, transform, screen, color ):
-    pass
+    fd = open(fname,'r')
+    data = (fd.read()).splitlines()
+    i = 0
+    while i < len(data):
+        if data[i] == "quit":
+            break
+        if data[i] == "line":
+            i += 1
+            a = data[i].split(" ")
+            if len(a) != 6:
+                print("Not enough data: line " + str(i))
+                break
+            add_edge(points,int(a[0]),int(a[1]),int(a[2]),int(a[3]),int(a[4]),int(a[5]))
+        if data[i] == "scale":
+            i += 1
+            a = data[i].split(" ")
+            if len(a) != 3:
+                print("Not enough data: line " + str(i))
+                break
+            m = make_scale(int(a[0]),int(a[1]),int(a[2]))
+            transform = matrix_mult(m,transform)
+        if data[i] == "translate":
+            i += 1
+            a = data[i].split(" ")
+            if len(a) != 3:
+                print("Not enough data: line " + str(i))
+                break
+            m = make_translate(int(a[0]),int(a[1]),int(a[2]))
+            transform = matrix_mult(m,transform)
+        if data[i] == "rotate":
+            i += 1
+            a = data[i].split(" ")
+            if len(a) != 2:
+                print("Not enough data: line " + str(i))
+                break
+            if a[0] == "x":
+                m = make_rotX(int(a[1]))
+            if a[0] == "y":
+                m = make_rotY(int(a[1]))
+            if a[0] == "z":
+                m = make_rotZ(int(a[1]))
+        if data[i] == "apply":
+            points = matrix_mult(transform,points)
+            transform = ident(transform)
+        if data[i] == "display":
+            draw_lines(points,screen,color)
+            display(screen)
+        if data[i] == "save":
+            draw_lines(points,screen,color)
+            i += 1
+            a = data[i].split(" ")
+            if len(a) != 1:
+                print("Not enough data: line " + str(i))
+                break
+            save_ppm(screen,a[0])
+        i+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
